85c5bf97c62a24cd12ae678bc0b51c574c67fd47
Logan Shy <loganshy740@gmail.com>
2020-05-24 16:26:05 -0600

    implemented api for posting new time and add timeSinceFed variable

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 80
+var timeSinceFed = "defaultTimeText"
 
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({extended: true})) //for parsing application/x-www-form-urlencoded
 
-var timeSinceFed = "defaultTimeText"
+app.get('/', (req, res) => res.send(timeSinceFed)) //Send current TSF in response to GET request at root URL
 
-app.get('/', (req, res) => res.send(timeSinceFed))
-
-app.post('/', (req, res) => {
-    timeSinceFed = req.body.time
-    console.log(req.body)
-    res.send(`POST request received: ${timeSinceFed}`)
+app.post('/', (req, res) => {   //Update current TSF with kvp from POST body and echo time back
+    if(req.body.time == null){
+        res.send(`POST request formatted incorrectly`)
+    } else {
+        timeSinceFed = req.body.time
+        console.log(req.body)
+        res.send(`POST request received: ${timeSinceFed}`)
+    }
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`))
