3fe74e52478c23720734bd56804a422effcf79a5
Logan Shy <loganshy740@gmail.com>
2020-05-30 16:17:44 -0600

    formatted GET response as JSON object

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var timeSinceFed = "defaultTimeText"
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({extended: true})) //for parsing application/x-www-form-urlencoded
 
-app.get('/', (req, res) => res.send(timeSinceFed)) //Send current TSF in response to GET request at root URL
+app.get('/', (req, res) => res.json({ time: timeSinceFed})) //Send current TSF in JSON format as response to GET request at root URL
 
 app.post('/', (req, res) => {   //Update current TSF with kvp from POST body and echo time back
     if(req.body.time == null){
